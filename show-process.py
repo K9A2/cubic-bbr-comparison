86e3422a5c1bfe11c9ef0e894992f6cf34de4e22
stormlin <lin-jinting@qq.com>
2018-12-04 20:02:48 +0800

    Refactor the code to new experiment cofigurations

--- show-process.py
+++ show-process.py
@@ -6,25 +6,25 @@ import libstorm as storm
 
 def main():
 
-  rtt = ['12']
-  loss = ['0.01', '0.1', '1', '3', '5']
+    # rtt = ['12', '30', '60', '100', '200', '300']
+    rtt = ['100', '200', '300']
+    loss = ['0.01', '0.05', '0.1', '1.0', '3.0', '5.0']
 
-  file_name_template = 'runtime-rtt%s-loss%s.log'
+    file_name_template = 'runtime-rtt%s-loss%s.log'
 
-  storm.log('-------- Evaluation Process --------')
+    storm.log('-------- Evaluation Process --------')
 
-  for r in rtt:
-    for l in loss:
-      file_name = file_name_template % (r, l)
-      lines = storm.read_text_file(file_name)
-      length = len(lines)
-      if length > 11:
-        storm.log(file_name + ' DONE')
-      else:
-        storm.log(file_name + ' ROUND-' + str(length))
-    storm.log('------------------------------------')
+    for r in rtt:
+        for l in loss:
+            file_name = file_name_template % (r, l)
+            lines = storm.read_text_file(file_name)
+            length = len(lines)
+            if length > 10:
+                storm.log(file_name + ' DONE')
+            else:
+                storm.log(file_name + ' ROUND-' + str(length))
+        storm.log('------------------------------------')
 
 
 if __name__ == '__main__':
-  main()
-
+    main()
