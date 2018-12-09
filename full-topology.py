f351f10f9c7a84ac308a28ab1ab62ba4e6933ef3
stormlin <lin-jinting@qq.com>
2018-12-09 14:40:14 +0800

    Enable higher concurrency to fully use hardware

--- full-topology.py
+++ full-topology.py
@@ -14,15 +14,26 @@ from mininet.topo import Topo
 import libstorm as storm
 
 
-loss = [0.01, 0.05, 0.10, 0.20, 0.40, 0.60, 0.80, 1.00, 3.00, 5.00]
-# rtt = [12, 30, 60, 100, 200, 300]
-rtt = [100, 200, 300]
+loss = [0.01, 0.10, 1.00]
+rtt = [30, 100, 200]
 
-algorithm = 'illinois'
+algorithm = 'cubic'
+
+max_test_count = 120
+# Tests conducted in each group
+step = 10
+# The start point of each group
+group_start = []
+
+i = 0
+while i < max_test_count:
+    group_start.append(i)
+    print i
+    i += step
 
 tcpdump_command_template = \
     'tcpdump -i %s-eth0 tcp 1> ./tcpdump-%s-rtt%s-loss%s-1g-%s.log &'
-shell_command_template = './run.sh -c %s -a %s -r %s -l %s 1> %s &'
+shell_command_template = './run.sh -c %s -a %s -r %s -l %s -s %s -e %s 1> %s &'
 
 
 class FullTopology(Topo):
@@ -30,32 +41,33 @@ class FullTopology(Topo):
     def __init__(self):
         Topo.__init__(self)
 
-        i = 0
-        # 按照设定的 RTT 和丢包率来依次建立拓扑
+        # set up the topology according loss and rtt specified
+        j = 0
         for r in rtt:
             for l in loss:
-                # 主机和交换机名称
-                sender_name = 'h' + str(i)
-                receiver_name = 'h' + str(i + 1)
+                for i in range(0, len(group_start)):
+                    # the name of sender and receiver hosts
+                    sender_name = 'h' + str(j) + '-1'
+                    receiver_name = 'h' + str(j) + '-2'
 
-                sender_switch_name = 's' + str(i)
-                receiver_switch_name = 's' + str(i + 1)
+                    sender_switch_name = 's' + str(j) + '-1'
+                    receiver_switch_name = 's' + str(j) + '-2'
 
-                i += 2
+                    j += 1
 
-                # 添加收发双方主机和路由器
-                sender = self.addHost(sender_name)
-                sender_switch = self.addSwitch(sender_switch_name)
-                receiver = self.addHost(receiver_name)
-                receiver_switch = self.addSwitch(receiver_switch_name)
+                    # add hosts and switches
+                    sender = self.addHost(sender_name)
+                    sender_switch = self.addSwitch(sender_switch_name)
+                    receiver = self.addHost(receiver_name)
+                    receiver_switch = self.addSwitch(receiver_switch_name)
 
-                # 连接各主机和路由器
-                self.addLink(sender, sender_switch,
-                             bw=1000, delay=str(r / 6) + 'ms', loss=0)
-                self.addLink(sender_switch, receiver_switch,
-                             bw=100, delay=str(r / 6) + 'ms', loss=l)
-                self.addLink(receiver_switch, receiver,
-                             bw=1000, delay=str(r / 6) + 'ms', loss=0)
+                    # connect the devices and create bottleneck
+                    self.addLink(sender, sender_switch,
+                                 bw=1000, delay=str(r / 6) + 'ms', loss=0)
+                    self.addLink(sender_switch, receiver_switch,
+                                 bw=100, delay=str(r / 6) + 'ms', loss=l)
+                    self.addLink(receiver_switch, receiver,
+                                 bw=1000, delay=str(r / 6) + 'ms', loss=0)
 
 
 def run():
@@ -63,42 +75,45 @@ def run():
     net = Mininet(topo=FullTopology(), host=CPULimitedHost, link=TCLink)
     net.start()
 
-    # 在运行前设置各主机
-    i = 0
+    # configure the network and run the test
+    j = 0
     for r in rtt:
         for l in loss:
-            sender_name = 'h' + str(i)
-            receiver_name = 'h' + str(i + 1)
-            i += 2
+            for i in range(0, len(group_start)):
+                sender_name = 'h' + str(j) + '-1'
+                receiver_name = 'h' + str(j) + '-2'
 
-            sender = net.get(sender_name)
-            receiver = net.get(receiver_name)
+                j += 1
 
-            # 关闭所有主机的 tso 功能
-            sender.cmd('ethtool -K ' + sender_name + '-eth0 tso off')
-            receiver.cmd('ethtool -K ' + receiver_name + '-eth0 tso off')
+                sender = net.get(sender_name)
+                receiver = net.get(receiver_name)
 
-            time.sleep(3)
+                # disable tso at all hosts
+                sender.cmd('ethtool -K ' + sender_name + '-eth0 tso off')
+                receiver.cmd('ethtool -K ' + receiver_name + '-eth0 tso off')
 
-            # 在接收方开启 iperf daemon
-            receiver.cmd('iperf3 -s -D')
+                time.sleep(3)
 
-            # 在收发双方进行 tcpdump 抓包
-            sender.cmd(tcpdump_command_template % (
-                sender_name, algorithm, str(r), str(l), 'sender'))
-            receiver.cmd(tcpdump_command_template % (
-                receiver_name, algorithm, str(r), str(l), 'receiver'))
+                # run iperf3 daemon at the receiver
+                receiver.cmd('iperf3 -s -D')
 
-            storm.log('sleep 3s to run the test')
-            time.sleep(3)
+                storm.log('sleep 3s to run the test')
+                time.sleep(3)
 
-            # run the test
-            sender.cmd(shell_command_template % (
-                algorithm, receiver.IP(), str(r), str(l),
-                'runtime-rtt' + str(r) + '-loss' + str(l) + '.log'
-            ))
+                # run the test
+                sender.cmd(shell_command_template % (
+                    # network information
+                    algorithm, receiver.IP(), str(r), str(l),
+                    # the start round and end round of this group
+                    str(group_start[i]), str(group_start[i] + step),
+                    # log file name
+                    algorithm + '-runtime-rtt' + str(r) + '-loss' + str(l) +
+                    '-' + str(i) + '.log'
+                ))
 
-            storm.log('rtt' + str(r) + '-loss' + str(l) + ' started')
+                storm.log(algorithm + ': rtt' + str(r) + '-loss' + str(l) +
+                          '-' + str(i) + '-' + str(group_start[i]) + '-' +
+                          str(group_start[i] + step) + ' started')
 
     CLI(net)
     net.stop()
